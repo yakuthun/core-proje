295d73be0147996d14a596cf4820f8d22eda6035
yakuthun <yakuthun@gmail.com>
2022-07-16 01:29:05 +0300

    theme changed

---
+++ core-proje/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace core_proje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public void NavigationBar(string v1, string v2, string v3)
+        {
+            ViewBag.v1 = v1;
+            ViewBag.v2 = v2;
+            ViewBag.v3 = v3;
+        }
+        public IActionResult Index()
+        {
+            NavigationBar("Öne Çıkan Listesi", "Öne Çıkanlar", "Öne Çıkan Listesi");
+            return View();
+        }
+    }
+}
