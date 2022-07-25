2f8fafe41e3b05bf78155cd15318142a23355992
yakuthun <yakuthun@gmail.com>
2022-07-25 03:19:59 +0300

    admin details finish

---
+++ core-proje/Controllers/AdminMessageController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace core_proje.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList()
+        {
+            string p = "admin@gmail.com";
+            var values = writerMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult SenderMessageList()
+        {
+            string p = "admin@gmail.com";
+            var values = writerMessageManager.GetListSenderMessage(p);
+            return View(values);
+        }
+        public IActionResult AdminMessageDetails(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            return View(values);
+        }
+        public IActionResult DeleteAdminMessage(int id)
+        {
+            var values = writerMessageManager.TGetByID(id);
+            writerMessageManager.TDelete(values);
+            return RedirectToAction("SenderMessageList");
+        }
+
+        [HttpGet]
+        public IActionResult AddAdminMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddAdminMessage(WriterMessage p)
+        {
+            p.Sender = "admin@gmail.com";
+            p.SenderName = "Admin";
+            p.Date = DateTime.Parse(DateTime.Now.ToString());
+            Context c = new Context();
+            var usernamesurname = c.Users.Where(x => x.Email == p.Receiver).Select(y => y.Name + " " + y.Surname).FirstOrDefault();
+            p.ReceiverName = usernamesurname;
+            writerMessageManager.TAdd(p);
+            return RedirectToAction("SenderMessageList");
+        }
+    }
+}
