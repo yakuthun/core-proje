e3f7a9b7466fb574abd57aabbbb8b2b91842f1d7
yakuthun <yakuthun@gmail.com>
2022-07-25 06:29:43 +0300

    social media references finish

---
+++ core-proje/Controllers/TestimonialController.cs
@@ -0,0 +1,46 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace core_proje.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IActionResult Index()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+        public IActionResult DeleteTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            testimonialManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult UpdateTestimonial(int id)
+        {
+            var values = testimonialManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public IActionResult UpdateTestimonial(Testimonial p)
+        {
+            testimonialManager.TUpdate(p);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult AddTestimonial()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTestimonial(Testimonial p)
+        {
+            testimonialManager.TAdd(p);
+            return RedirectToAction("Index");
+        }
+    }
+}
