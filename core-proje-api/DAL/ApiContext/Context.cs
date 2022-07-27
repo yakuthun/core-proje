2e97540b61883e952558f65df030c470a2133314
yakuthun <yakuthun@gmail.com>
2022-07-27 14:19:29 +0300

    apis finish

---
+++ core-proje-api/DAL/ApiContext/Context.cs
@@ -0,0 +1,14 @@
+﻿using core_proje_api.DAL.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace core_proje_api.DAL.ApiContext
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-0NEP572;database=CoreProjeDB2;integrated security=true");
+        }
+        public DbSet<Category> Categories{ get; set; }
+    }
+}
---
+++ core-proje-api/DAL/Entity/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace core_proje_api.DAL.Entity
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
